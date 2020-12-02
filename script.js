6e4c18b2997cebf90c4ef398dd31ea15af9333e6
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-12-02 15:30:56 -0500

    Adds helper function to clean up code in displayImages

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ let imagesArray = [];
 const apiKey = `YOUR_API_KEY`;
 const unsplashUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${initialImageCount}`;
 
+// Helper function to set attributes
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements for links and images. Add to DOM
 function displayImages() {
   // Loop through each object in imagesArray
   imagesArray.forEach((image) => {
     // Create <a> for link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", image.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: image.links.html,
+      target: "_blank",
+    });
     // Create <img> for Unsplash images
     const img = document.createElement("img");
-    img.setAttribute("src", image.urls.regular);
-    img.setAttribute("alt", image.alt_description);
-    img.setAttribute("title", image.alt_description);
+    setAttributes(img, {
+      src: image.urls.regular,
+      alt: image.alt_description,
+      title: image.alt_description,
+    });
     // Place <img> inside <a> then add both to imageContainer
     item.appendChild(img);
     imageContainer.appendChild(item);
